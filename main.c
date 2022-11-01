2a07e4e37c219e4f140f20a365f5b1c2ab41a32c
Amanda Gomes <88012982+AmandaGomesFe@users.noreply.github.com>
2022-11-01 18:22:56 -0300

    exercicio de array

--- main.c
+++ main.c
@@ -1,17 +1,32 @@
-/*****************************************************************************
+/******************************************************************************
 
-
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
 #include <stdio.h>
+# define TAMANHO 5
 
+int v[TAMANHO], s = 0, n, media;
+
+void soma(n){
+    s = s + n;
+    printf("soma: ", s);
+}
 int main()
-{
-    float a = 7, b = 2;
-    printf("A soma de %.2f e %.2f é %.2f \n", a, b, a + b);
-    printf("A subtração é %.2f \n", a - b);
-    printf("A divisao é %.2f \n", a / b);
-    printf("A multiplicação é %.2f \n", a * b);
+{   
+    for(int i = 0; i < TAMANHO; i++){
+        printf("Informe um número: ");
+        scanf("%d", &v[TAMANHO]); 
+        soma(v[TAMANHO]);
+    }
+    
+    
+    
+    
+    
 
     return 0;
 }
